b62a98a6027dd2504ace0eeea5182a31eb2ba102
Kane Wang <kane@yeecord.com>
2026-03-24 01:49:01 +0800

    simplify `outputFileTracingIncludes`

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/*": ["node_modules/@takumi-rs/core-*/*"],
+  },
   turbopack: {
     root: import.meta.dirname,
-  },
-  outputFileTracingIncludes: {
-    "/*": [
-      "node_modules/@takumi-rs/core-darwin-x64/**/*",
-      "node_modules/@takumi-rs/core-darwin-arm64/**/*",
-      "node_modules/@takumi-rs/core-linux-arm64-gnu/**/*",
-      "node_modules/@takumi-rs/core-linux-arm64-musl/**/*",
-      "node_modules/@takumi-rs/core-win32-arm64-msvc/**/*",
-      "node_modules/@takumi-rs/core-linux-x64-gnu/**/*",
-      "node_modules/@takumi-rs/core-linux-x64-musl/**/*",
-      "node_modules/@takumi-rs/core-win32-x64-msvc/**/*",
-    ],
   },
   experimental: {
     turbopackFileSystemCacheForDev: true,
