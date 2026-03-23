7a7c0040cf6e7d3f6542ff3d1334f7ed4e322528
Kane Wang <kane@yeecord.com>
2026-03-24 01:17:29 +0800

    static `outputFileTracingIncludes`

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,20 @@
-import { readFileSync } from "node:fs";
-import { dirname, join } from "node:path";
 import type { NextConfig } from "next";
-
-const takumiCoreEntryPath = import.meta.resolve("@takumi-rs/core");
-const { optionalDependencies } = JSON.parse(
-  readFileSync(join(dirname(takumiCoreEntryPath), "package.json"), "utf8"),
-) as { optionalDependencies?: Record<string, string> };
 
 const nextConfig: NextConfig = {
   turbopack: {
     root: import.meta.dirname,
   },
   outputFileTracingIncludes: {
-    "/*": Object.keys(optionalDependencies ?? {}).map((pkg) => `./node_modules/${pkg}/**/*`),
+    "/*": [
+      "node_modules/@takumi-rs/core-darwin-x64/**/*",
+      "node_modules/@takumi-rs/core-darwin-arm64/**/*",
+      "node_modules/@takumi-rs/core-linux-arm64-gnu/**/*",
+      "node_modules/@takumi-rs/core-linux-arm64-musl/**/*",
+      "node_modules/@takumi-rs/core-win32-arm64-msvc/**/*",
+      "node_modules/@takumi-rs/core-linux-x64-gnu/**/*",
+      "node_modules/@takumi-rs/core-linux-x64-musl/**/*",
+      "node_modules/@takumi-rs/core-win32-x64-msvc/**/*",
+    ],
   },
   experimental: {
     turbopackFileSystemCacheForDev: true,
