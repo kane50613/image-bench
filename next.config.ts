a87c818d3fb2c47e5f01c89e15432242963cbb59
Kane Wang <kane@yeecord.com>
2026-04-26 15:36:26 +0800

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["takumi-js"],
+  serverExternalPackages: ["@takumi-rs/core"],
   turbopack: {
     root: import.meta.dirname,
   },
